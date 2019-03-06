b6b17f05bc96eb8a74ce329ddaf46ee0eb3ea854
Valentina <mimac1974@gmail.com>
2019-03-06 15:57:03 +0100

    Dodate potrebne aktivnosti

---
+++ app/src/main/java/projekat/pmaiu/androidprojekat/ContactsActivity.java
@@ -0,0 +1,43 @@
+package projekat.pmaiu.androidprojekat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContactsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+}
