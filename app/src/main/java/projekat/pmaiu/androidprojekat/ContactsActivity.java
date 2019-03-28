74595d522d2b6076ab59348938ef437514b517b2
Valentina <mimac1974@gmail.com>
2019-03-28 18:33:39 +0100

    Meni za contacts i ikona za createContact dodati

--- app/src/main/java/projekat/pmaiu/androidprojekat/ContactsActivity.java
+++ app/src/main/java/projekat/pmaiu/androidprojekat/ContactsActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -82,7 +83,6 @@ public class ContactsActivity extends AppCompatActivity implements NavigationVie
         } else{
             super.onBackPressed();
         }
-
     }
 
     @Override
@@ -107,6 +107,21 @@ public class ContactsActivity extends AppCompatActivity implements NavigationVie
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.contacts_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.action_new_contact)
+            startActivity(new Intent(ContactsActivity.this, CreateContactActivity.class));
+
         return true;
     }
 }
