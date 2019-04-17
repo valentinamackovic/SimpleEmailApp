2bad6fbc827465f01369ee0af352d72b7112d49f
valentinamackovic <mimac1974@gmail.com>
2019-04-17 14:36:46 +0200

    Ispravljena klasa Contact

--- app/src/main/java/model/Contact.java
+++ app/src/main/java/model/Contact.java
@@ -9,22 +9,61 @@ public class Contact {
     private String lastName;
     private String email;
     private Photo photo;
+    private String display;
+    private String format;
     private ArrayList<Message> messagesTo = new ArrayList<>();
     private ArrayList<Message> messagesFrom =  new ArrayList<>();
+    private ArrayList<Message> messagesCc = new ArrayList<>();
+    private ArrayList<Message> messagesBcc = new ArrayList<>();
 
     public Contact() {
         super();
     }
 
-    public Contact(int id, String firstName, String lastName, String email,
-                   Photo photo, ArrayList<Message> messagesTo, ArrayList<Message> messagesFrom) {
+    public Contact(int id, String firstName, String lastName, String email, Photo photo, String display, String format, ArrayList<Message> messagesTo, ArrayList<Message> messagesFrom, ArrayList<Message> messagesCc, ArrayList<Message> messagesBcc) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.photo = photo;
+        this.display = display;
+        this.format = format;
         this.messagesTo = messagesTo;
         this.messagesFrom = messagesFrom;
+        this.messagesCc = messagesCc;
+        this.messagesBcc = messagesBcc;
+    }
+
+    public void setDisplay(String display) {
+        this.display = display;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public String getDisplay() {
+        return display;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setMessagesCc(ArrayList<Message> messagesCc) {
+        this.messagesCc = messagesCc;
+    }
+
+    public void setMessagesBcc(ArrayList<Message> messagesBcc) {
+        this.messagesBcc = messagesBcc;
+    }
+
+    public ArrayList<Message> getMessagesCc() {
+        return messagesCc;
+    }
+
+    public ArrayList<Message> getMessagesBcc() {
+        return messagesBcc;
     }
 
     public ArrayList<Message> getMessagesTo() {
